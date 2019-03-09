849bdd121b76ca2f5cbc64429249b74328273288
jessie-zly <610141748@qq.com>
2019-03-09 14:08:34 +0800

    重构

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <div class="loading"></div>
+    <div class="nav"></div>
+    <div class="container">
+      <div class="c-screen"></div>
+      <div class="c-article"></div>
+      <div class="c-page"></div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+// @ is an alias to /src
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Component({
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="less" scoped>
+.container{
+  // position: relative;
+  .c-screen{
+    width: 100%;
+    height: 100%;
+    background: url('https://isujin.com/wp-content/uploads/2018/11/wallhaven-672007-1.png') no-repeat center;
+  }
+}
+</style>
---
+++ src/views/Toast.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="mask">
+    <div class="menu">
+      <div class="menu-top">
+        <span class="menu-logo"></span>
+        <span class="menu-icon" v-show='isMenu' @click='handleMenu'></span>
+        <span class="menu-icon menu-icon-x" v-show='!isMenu' @click='handleMenu'></span>
+      </div>
+      <ul class="menu-middle">
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Links</a></li>
+        <li><a href="#">Archive</a></li>
+        <li><a href="#">Github</a></li>
+        <li><a href="#">Music</a></li>
+        <li><a href="#">Life</a></li>
+      </ul>
+      <p class="menu-bottom">Designed By Jessie· 2019 © 雷雨</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+// 引入组件方式
+// @Component({
+//   components: {
+//     HelloWorld
+//   }
+// })
+
+@Component
+export default class Mask extends Vue {
+  // data
+  isMenu: boolean = true
+
+  // props
+  @Prop() private msg!: string
+
+  // mounted
+  mounted () {
+    //
+  }
+
+  // computed
+  get xxx ():string {
+    return 'xxx'
+  }
+
+  // methods
+  handleMenu ():void{
+    this.isMenu = !this.isMenu
+  }
+}
+</script>
+
+<style scoped lang="less">
+.mask{
+  .menu{
+    .menu-top{
+      width: 80%;
+      margin: 0 auto 0;
+      display: flex;
+      justify-content: space-between;
+      .menu-logo{
+        display: block;
+        width: 20px;
+        height: 20px;
+      }
+      .menu-icon{
+        display: block;
+        width: 20px;
+        height: 20px;
+        background: url('../assets/menu.png') no-repeat center;
+      }
+      .menu-icon-x{
+        background: url('../assets/close.png') no-repeat center;
+      }
+    }
+    ul{
+      width: 60%;
+      margin: 160px auto 0;
+      display: flex;
+      justify-content: space-around;
+      text-align: center;
+      li{
+        list-style: none;
+        font-size: 16px;
+        margin: 10px 10px;
+        a{
+          text-decoration: none;
+          color: #999;
+          letter-spacing: 1px;
+        }
+        a:hover{
+          color:#666;
+        }
+      }
+    }
+    p{
+      width: 100%;
+      color: #999;
+      font-size: 14px;
+      text-align: center;
+      position: absolute;
+      bottom: 30px;
+    }
+  }
+}
+</style>
